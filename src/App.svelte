188ab1864a7ab2aa87b6c760dcd08fd7c5ead5f9
tnehil <tom.nehil@gmail.com>
2025-03-18 17:07:29 +0000

    Starts the barebones app

--- src/App.svelte
+++ src/App.svelte
@@ -1,13 +1,37 @@
 <script>
+    import Route from "./Route.svelte";
+    let selectedRoute = $state("");
+    import routeData from "./data/routes.json"
 
-  let message = $state("Hello world!")
+    const slugify = (routeTitle) => routeTitle.toLowerCase().replace(/ /g,"-")
+    const slugs = routeData.map(r => slugify(r.headline))
+    let hash = $state(window.location.hash)
+    
+    $effect(()=> {
+        if (hash) {
+            console.log(hash.replace("#/",""))
+            if (slugs.includes(hash.replace("#/",""))) {
+                selectedRoute = routeData.filter(r => slugify(r.headline) === hash.replace("#/",""))[0]?.headline
+            }
+            else {
+                window.location.hash = ""
+                window.location.href = window.location.href.replace("#","")
+            }
+        } else {
+            selectedRoute = ""
+        }
+    })
 
 </script>
-
+<svelte:window on:hashchange={()=>{hash=window.location.hash}} />
 <main class="px-4 lg:px-20 pt-8">
-  <h1 class="font-editorial-news-heading-02">{message}</h1>
+    {#if selectedRoute}
+        <Route routeData={routeData.filter(r => r.headline === selectedRoute)[0]}/>
+    {:else}
+    <ul>
+    {#each routeData as route}
+        <li><a href="#/{slugify(route.headline)}">{slugify(route.headline)}</a></li>
+    {/each}
+    </ul>
+    {/if}
 </main>
-
-<style>
-
-</style>
---
+++ src/Route.svelte
@@ -0,0 +1,24 @@
+<script>
+    let {routeData} = $props()
+    let {
+        headline,
+        heroImg,
+        heroImgCredit,
+        subhead,
+        tags,
+        authors,
+        summary,
+        timeToComplete,
+        nearbyCities,
+        recommendedConditions,
+        mapUrl,
+        narrative,
+        knowBeforeYouGo,
+        Photos,
+        routeID
+    } = $derived(routeData)
+
+    $inspect(routeData)
+</script>
+
+<h1 class="font-utility-heading-01">{headline}</h1>
