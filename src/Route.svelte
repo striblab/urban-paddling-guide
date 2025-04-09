0058e12b9aa4c2d556b6df9cf9836da8f893ee45
tnehil <tom.nehil@gmail.com>
2025-04-09 14:18:03 -0500

    formatting cleanup

--- src/Route.svelte
+++ src/Route.svelte
@@ -33,21 +33,21 @@
             return `<span class="author">${authors[0]}</span>`;
         if (authors.length === 2)
             return `<span class="author">${authors.join('</span> and <span class="author">')}</span>`;
-        return `<span class="author">${authors.slice(0, -1).join('</span>, <span class="author">')}</span> and <span class="author">${authors[authors.length-1]}</span>`;
+        return `<span class="author">${authors.slice(0, -1).join('</span>, <span class="author">')}</span> and <span class="author">${authors[authors.length - 1]}</span>`;
     };
 
-    const getKnowMarkup = (text) => {
+    const getKnowMarkup = (text) => {};
 
-    }
-
-    onMount(()=>{
-        topElement.scrollIntoView()
-        const pymParent = new Parent("pymFrame", mapUrl, {})
+    onMount(() => {
+        topElement.scrollIntoView();
+        const pymParent = new Parent("pymFrame", mapUrl, {});
     });
 </script>
 
 <div class="route w-full" bind:this={topElement}>
-    <div class="headline-wrapper text-center w-[90%] mx-auto max-w-2xl pt-[10vh]">
+    <div
+        class="headline-wrapper text-center w-[90%] mx-auto max-w-2xl pt-[10vh]"
+    >
         <h1
             class="font-publico-headline-black text-[35px] md:text-[50px] md:mb-6 mb-4"
         >
@@ -66,9 +66,15 @@
         </div>
     </div>
 
-    <div class="w-[90%] flex-module mx-auto max-w-6xl md:flex border-[#05442e] py-6 my-12 justify-start border-t-2 pt-8 text-center">
-        <div class="completion-time item border-b md:border-b-0 md:border-r border-[#05442e] w-full md:w-1/3 mb-6 md:mb-0 pb-6 md:px-8 md:pl-0">
-            <h5 class="font-graphik-bold uppercase tracking-widest text-[14px] mb-2" >
+    <div
+        class="w-[90%] flex-module mx-auto max-w-6xl md:flex border-[#05442e] py-6 my-12 justify-start border-t-2 pt-8 text-center"
+    >
+        <div
+            class="completion-time item border-b md:border-b-0 md:border-r border-[#05442e] w-full md:w-1/3 mb-6 md:mb-0 pb-6 md:px-8 md:pl-0"
+        >
+            <h5
+                class="font-graphik-bold uppercase tracking-widest text-[14px] mb-2"
+            >
                 Time to complete
             </h5>
             <p class="font-graphik-regular text-[18px]">{timeToComplete}</p>
@@ -100,7 +106,7 @@
             </p>
         </div>
     </div>
-    
+
     <div
         class="hero-photo w-full h-[60vh] mb-8 md:mb-12 md:max-w-full md:h-screen md:mx-auto relative"
         style="background: url({heroImg}); background-size: cover;background-position: 50% 50%;"
@@ -122,15 +128,10 @@
         </div>
     </div>
 
-    
     {#if mapUrl}
-            <div class="map mt-8 mb-8 w-[90%] mx-auto max-w-2xl">
-                <div
-                    title="route map"
-                    id="pymFrame"
-                    style="width: {mapWidth === "vertical" ? 672 : 1200}px;"
-                ></div>
-            </div>
+        <div class="map mt-8 mb-8 w-[90%] mx-auto max-w-2xl">
+            <div title="route map" id="pymFrame" style="width: 110%;"></div>
+        </div>
     {/if}
 
     <div class="narrative w-[90%] mx-auto max-w-2xl">
@@ -144,7 +145,7 @@
     </div>
 
     <div class="photos w-full md:w-[90%] mx-auto max-w-6xl mt-12 mb-12">
-        <img src="https://placehold.co/1200x800" />
+        <img src="https://placehold.co/1200x800" alt="placeholder"/>
         <p
             class="caption w-[90%] mx-auto max-w-2xl font-graphik-regular text-[14px] mt-2"
         >
