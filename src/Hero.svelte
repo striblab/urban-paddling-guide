342546f5a762b9df17afa1d78fea64282606a6cf
tnehil <tom.nehil@gmail.com>
2025-04-15 16:30:34 -0500

    Changes h2s to ps in hero and applies formatting

--- src/Hero.svelte
+++ src/Hero.svelte
@@ -2,67 +2,134 @@
 </script>
 
 <div class="hero bg-[#fffbf4] pb-20">
-	<div class="video-wrapper relative w-full">
-		<video src="https://static.startribune.com/assets/outdoors/urban-paddling/MinneapolisMississippi_1_1.mp4" autoplay muted loop playsinline id="bgVideo">
-		</video>
-	</div>
-	<div class="subhead w-[90%] mx-auto">
-		<h1 class="font-publico-banner-black text-[40px] md:text-[60px] text-center mx-auto text-white mt-[-100vh] pt-[33vh] md:pt-[38vh] max-w-4xl">Your ultimate guide to paddling in the Twin Cities</h1>
-		<div class="font-publico-text-roman text-[18px] md:text-[20px] leading-[1.6] text-left max-w-xl mx-auto p-8 bg-white mb-[110vh] mt-[100vh] rounded-xl">
-			<p class="byline text-center font-graphik-semibold text-[16px] mb-1"><span class="font-graphik-bold">By Jake Steinberg, Tom Nehil and Greta Kaul</span></p>
-	      	<p class="byline text-center uppercase font-graphik-regular text-[13px] tracking-widest mb-8">The Minnesota Star Tribune •  May XX, 2025</p>
-			<h2 class="mb-6">You don’t have to go to the Boundary Waters to have fun paddling a canoe.</h2>
-			<h2 class="mb-6">In Minnesota, we pride ourselves on being connected to water. But getting out there in the Twin Cities metro area is easier said than done. Where do I put in my boat? How fast is too fast for moving water? How long will this take? Are there rentals? The information you need is all over the place, if you can find it at all.</h2>
-			<h2 class="mb-6">We think it could be easier, so some water-loving Minnesota Star Tribune staff members set out to create an urban paddling guide. We researched, paddled and mapped our favorite routes within the metro area.</h2>
-			<h2 class="mb-6">Whether by canoe, kayak or paddleboard, we hope this guide helps you see the Twin Cities in new ways: take a lazy river ride toward the Minneapolis skyline; explore a little-seen part of St. Paul; practice portaging at Lebanon Hills in the southern suburbs.</h2>
-			<h2 class="mb-6">While you’re out there, don’t forget to:
-				<ul>
-					<li class="ml-4 md:ml-6"><span class="font-publico-headline-black">Bring a life jacket.</span> State law requires one for each person on board your watercraft.</li>
-					<li class="ml-4 md:ml-6"><span class="font-publico-headline-black">Think twice</span> before paddling in winds over 10 mph.</li>
-					<li class="ml-4 md:ml-6"><span class="font-publico-headline-black">Watch water levels</span> on rivers and creeks.</li>
-					<li class="ml-4 md:ml-6"><span class="font-publico-headline-black">Swim with care.</span></li>
-					<li class="ml-4 md:ml-6"><span class="font-publico-headline-black">Be mindful</span> of private property.</li>
-				</ul>
-			</h2>
-			<h2 class="">See you on the water, and please tag @startribune in your urban paddling photos.</h2>
-		</div>
-	</div>
+    <div class="video-wrapper relative w-full">
+        <video
+            src="https://static.startribune.com/assets/outdoors/urban-paddling/MinneapolisMississippi_1_1.mp4"
+            autoplay
+            muted
+            loop
+            playsinline
+            id="bgVideo"
+        >
+        </video>
+    </div>
+    <div class="subhead w-[90%] mx-auto">
+        <h1
+            class="font-publico-banner-black text-[40px] md:text-[60px] text-center mx-auto text-white mt-[-100vh] pt-[33vh] md:pt-[38vh] max-w-4xl"
+        >
+            Your ultimate guide to paddling in the Twin Cities
+        </h1>
+        <div
+            class="font-publico-text-roman text-[18px] md:text-[20px] leading-[1.6] text-left max-w-xl mx-auto p-8 bg-white mb-[110vh] mt-[100vh] rounded-xl"
+        >
+            <p
+                class="byline text-center font-graphik-semibold text-[16px] mb-1"
+            >
+                <span class="font-graphik-bold"
+                    >By Jake Steinberg, Tom Nehil and Greta Kaul</span
+                >
+            </p>
+            <p
+                class="byline text-center uppercase font-graphik-regular text-[13px] tracking-widest mb-8"
+            >
+                The Minnesota Star Tribune •  May XX, 2025
+            </p>
+            <p class="mb-6">
+                You don’t have to go to the Boundary Waters to have fun paddling
+                a canoe.
+            </p>
+            <p class="mb-6">
+                In Minnesota, we pride ourselves on being connected to water.
+                But getting out there in the Twin Cities metro area is easier
+                said than done. Where do I put in my boat? How fast is too fast
+                for moving water? How long will this take? Are there rentals?
+                The information you need is all over the place, if you can find
+                it at all.
+            </p>
+            <p class="mb-6">
+                We think it could be easier, so some water-loving Minnesota Star
+                Tribune staff members set out to create an urban paddling guide.
+                We researched, paddled and mapped our favorite routes within the
+                metro area.
+			</p>
+            <p class="mb-6">
+                Whether by canoe, kayak or paddleboard, we hope this guide helps
+                you see the Twin Cities in new ways: take a lazy river ride
+                toward the Minneapolis skyline; explore a little-seen part of
+                St. Paul; practice portaging at Lebanon Hills in the southern
+                suburbs.
+			</p>
+            <p >
+                While you’re out there, don’t forget to:
+            </p>
+			<ul class="mb-6">
+				<li class="ml-4 md:ml-6">
+					<span class="font-publico-headline-black"
+						>Bring a life jacket.</span
+					> State law requires one for each person on board your watercraft.
+				</li>
+				<li class="ml-4 md:ml-6">
+					<span class="font-publico-headline-black"
+						>Think twice</span
+					> before paddling in winds over 10 mph.
+				</li>
+				<li class="ml-4 md:ml-6">
+					<span class="font-publico-headline-black"
+						>Watch water levels</span
+					> on rivers and creeks.
+				</li>
+				<li class="ml-4 md:ml-6">
+					<span class="font-publico-headline-black"
+						>Swim with care.</span
+					>
+				</li>
+				<li class="ml-4 md:ml-6">
+					<span class="font-publico-headline-black"
+						>Be mindful</span
+					> of private property.
+				</li>
+			</ul>
+            <p class="">
+                See you on the water, and please tag @startribune in your urban
+                paddling photos.
+			</p>
+        </div>
+    </div>
 </div>
 
-
 <style type="text/css">
-	div.video-wrapper {
-		position: sticky;
-		width: 100%;
-		height: 100vh;
-		top: 0;
-		overflow: hidden;
-	}
-	h1 span {
-		box-shadow: inset 0px -20px #05442e;
-	}
-	li {
-		position: relative;
-	}
-	li:before {
-		content: '•';
-		position: absolute;
-		left: -15px;
-		height: 10px;
-	}
-	div.subhead {
-		z-index: 100;
-		position: relative;
-		margin: 0 auto;
-	}
-	div.video-wrapper video#bgVideo {
-		position: absolute;
-		left: 50%;
-		bottom: 0;
-		transform: translateX(-50%);
-		min-width: 100%;
-		min-height: 100%;
-		height: 100%;
-		max-width: none;
-	}
+    div.video-wrapper {
+        position: sticky;
+        width: 100%;
+        height: 100vh;
+        top: 0;
+        overflow: hidden;
+    }
+    h1 span {
+        box-shadow: inset 0px -20px #05442e;
+    }
+    li {
+        position: relative;
+    }
+    li:before {
+        content: "•";
+        position: absolute;
+        left: -15px;
+        height: 10px;
+    }
+    div.subhead {
+        z-index: 100;
+        position: relative;
+        margin: 0 auto;
+    }
+    div.video-wrapper video#bgVideo {
+        position: absolute;
+        left: 50%;
+        bottom: 0;
+        transform: translateX(-50%);
+        min-width: 100%;
+        min-height: 100%;
+        height: 100%;
+        max-width: none;
+    }
 </style>
