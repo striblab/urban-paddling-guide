db43dee3f9277264c705f038031c4d73639011ff
tnehil <tom.nehil@gmail.com>
2025-04-15 13:23:15 -0500

    Deletes errant quote messing up filter button

--- src/Filters.svelte
+++ src/Filters.svelte
@@ -63,7 +63,7 @@
         </div>
         <div class="tag leading-[0.7]">
             <button
-                class="inline-block p-2 mr-1 md:mr-2 mb-1 'text-[#05442e] hover:text-white bg-white
+                class="inline-block p-2 mr-1 md:mr-2 mb-1 text-[#05442e] hover:text-white bg-white
                     hover:bg-[#05442e] rounded-2xl border-[#05442e] border uppercase font-graphik-medium tracking-widest text-[12px] md:text-[14px]"
                 onclick={() => {
                     activeFilter = "";
