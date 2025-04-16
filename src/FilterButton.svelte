54499b125cdda0e947fa0606fe4d1b7b68e371c7
tnehil <tom.nehil@gmail.com>
2025-04-16 16:28:36 -0500

    Gets rid of some white in favor of the off-white

--- src/FilterButton.svelte
+++ src/FilterButton.svelte
@@ -4,9 +4,9 @@
 
 <div class="tag leading-[0.7]">
     <button
-        class="inline-block p-2 mr-1 md:mr-2 mb-1 {activeFilter!==tag
+        class="inline-block p-2 mr-1 md:mr-2 mb-1 {activeFilter !== tag
             ? 'text-[#05442e]'
-            : 'text-white'} hover:text-white {activeFilter!==tag
+            : 'text-[#fffbf4]'} hover:text-[#fffbf4] {activeFilter !== tag
             ? 'bg-[#fffbf4]'
             : 'bg-[#05442e]'} hover:bg-[#05442e] rounded-2xl border-[#05442e] border uppercase font-graphik-medium tracking-widest text-[12px] md:text-[14px]"
         onclick={() => {
