27bf93a1dd79abb810c5787ea297d6e19565a867
annaboone <anna.boone@startribune.com>
2025-05-05 14:14:32 -0500

    fix button styling, update popup type organization

--- src/map-components/Popup.svelte
+++ src/map-components/Popup.svelte
@@ -28,8 +28,10 @@
     <div
         class="absolute right-[5%] xl:right-[0px] bottom-[0%] md:bottom-[0px] z-10 w-[90%] md:w-[44%] lg:w-[32%] transition-all duration-500"
     >
-        <button class="float-right text-2xl text-red-500" onclick={closeRoute}>
-            X
+        <button class="absolute right-0 top-0 md:hover:opacity-50 transition-all duration-500 p-4 rounded-tr-xl md:p-4 " onclick={closeRoute}>
+            <svg id="strib-close" viewBox="0 0 16 16" width="16" height="16" fill="#05442e" class="strib-icon strib-close" xmlns="http://www.w3.org/2000/svg">
+              <path d="m8 7.037-5.5-5.5-.963.963 5.5 5.5-5.5 5.5.963.963 5.5-5.5 5.5 5.5.963-.963-5.5-5.5 5.5-5.5-.963-.963z"/>
+            </svg>
         </button>
         <div
             class="route-preview font-publico-headline-medium bg-white border rounded-xl border-[#05442e] overflow-hidden"
@@ -41,7 +43,7 @@
                 alt={popupData.heroImgAltText}
                 class="w-full hidden md:block"
             />
-            <div class="text-wrapper p-6 pt-14 md:pt-6">
+            <div class="text-wrapper p-6 py-14 md:pt-6">
                 <h3 class="text-[24px] md:text-[28px] mb-2 leading-[1.2]">
                     {popupData.headline}
                 </h3>
@@ -51,18 +53,18 @@
                     {popupData.subhead}
                 </h5>
                 <h3
-                    class="text-[16px] my-4 font-graphik-regular leading-[1.3] md:block hidden"
+                    class="text-[14px] md:text-[16px] my-4 font-graphik-regular leading-[1.3]"
                 >
                     {getFirstSentence(popupData.summary)}
                 </h3>
-                <a
-                    class="mb-1 mx-auto text-[#05442e] hover:opacity-50 border-[#05442e] border-b uppercase font-graphik-medium tracking-widest text-[12px] transition duration-500"
-                    href={"#/" + slugify(popupData.headline)}>View route</a
-                >
             </div>
         </div>
+        <a
+            class="mb-1 mx-auto text-[#05442e] hover:opacity-50 border-[#05442e] border-b uppercase font-graphik-medium tracking-widest text-[12px] transition duration-500 block absolute left-[50%] bottom-[10px] translate-x-[-50%]"
+            href={"#/" + slugify(popupData.headline)}>View route</a
+        >
         <button
-            class="absolute left-0 bottom-0 bg-[#05442e] hover:bg-[#fffbf4] transition-all duration-500 p-2 px-4 md:p-4 rounded-tl-xl border-[#05442e] border"
+            class="absolute left-0 bottom-0 bg-[#05442e] md:hover:bg-[#fffbf4] transition-all duration-500 p-2 px-4 md:p-4 rounded-bl-xl border-[#05442e] border"
             aria-label="previous route"
             onclick={() => {
                 loadRoute(prevRouteId);
@@ -83,7 +85,7 @@
             </svg>
         </button>
         <button
-            class="absolute right-0 bottom-0 bg-[#05442e] hover:bg-[#fffbf4] transition-all duration-500 p-2 px-4 md:p-4 rounded-tr-xl border-[#05442e] border"
+            class="absolute right-0 bottom-0 bg-[#05442e] md:hover:bg-[#fffbf4] transition-all duration-500 p-2 px-4 md:p-4 rounded-br-xl border-[#05442e] border"
             aria-label="next route"
             onclick={() => {
                 loadRoute(nextRouteId);
@@ -110,5 +112,13 @@
     button:hover svg path {
         fill: #05442e;
         transition: 0.5s all;
+    }
+    #strib-close path {
+        fill: white;
+    }
+    @media only screen and (max-width: 767px) {
+        #strib-close path {
+            fill: #05442e;
+        }
     }
 </style>
