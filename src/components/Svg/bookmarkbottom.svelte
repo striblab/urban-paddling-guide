afae71de48baec6502fdb1fa6c6239ebd1c8fceb
annaboone <anna.boone@startribune.com>
2026-06-29 16:27:40 -0500

    fix spacing issue

--- src/components/Svg/bookmarkbottom.svelte
+++ src/components/Svg/bookmarkbottom.svelte
@@ -1,24 +1,17 @@
 <svg id="Layer_1" xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 1200 400">
+  <!-- Generator: Adobe Illustrator 30.1.0, SVG Export Plug-In . SVG Version: 2.1.1 Build 136)  -->
   <defs>
     <style>
       .st0 {
         fill: #e36363;
       }
-
-      .st1 {
-        fill: none;
-        stroke: #e36363;
-        stroke-miterlimit: 10;
-        stroke-width: 10px;
-      }
     </style>
   </defs>
-  <polygon class="st0" points="4 6.67 4 400 594 6.67 1184 400 1184 6.67 4 6.67"/>
-  <polyline class="st1" points="4 6.67 4 400 594 6.67 1184 400 1184 6.67"/>
+  <polygon class="st0" points="0 0 0 400 600 0 1200 400 1200 0 0 0"/>
 </svg>
 
 <style>
   svg {
-    margin-top: -2px;
+    /* margin-top: -2px; */
   }
 </style>
