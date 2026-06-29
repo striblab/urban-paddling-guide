878661a14a56a29fc2386ee933e192567b8a2d9b
annaboone <anna.boone@startribune.com>
2026-06-29 15:27:32 -0500

    update credits

--- src/Credits.svelte
+++ src/Credits.svelte
@@ -7,11 +7,11 @@
         <div class="max-w-2xl">
             <h5 class="uppercase font-graphik-bold text-bold mb-2 text-[12px] md:text-[14px] tracking-widest">Credits</h5>
             <p class="font-graphik-regular text-[16px] mb-2"><span class="font-graphik-semibold inline-block pr-2">Writing</span>Jake Steinberg, Tom Nehil, Greta Kaul, C.J. Sinner, Susan Du, Walker Orenstein</p>
-            <p class="font-graphik-regular text-[16px] mb-2"><span class="font-graphik-semibold inline-block pr-2">Photography and Videography</span>Aaron Lavinsky, Anthony Soufflé, Greta Kaul</p>
+            <p class="font-graphik-regular text-[16px] mb-2"><span class="font-graphik-semibold inline-block pr-2">Photography and Videography</span>Aaron Lavinsky, Anthony Soufflé, Greta Kaul, Renée Jones Schneider</p>
             <p class="font-graphik-regular text-[16px] mb-2"><span class="font-graphik-semibold inline-block pr-2">Cartography</span>Jake Steinberg</p>
-            <p class="font-graphik-regular text-[16px] mb-2"><span class="font-graphik-semibold inline-block pr-2">Editing</span>Pam Louwagie, Tim Whitecotton, Genevieve Ross, Andy Putz, Mike Rice</p>
+            <p class="font-graphik-regular text-[16px] mb-2"><span class="font-graphik-semibold inline-block pr-2">Editing</span>Pam Louwagie, Tim Whitecotton, Genevieve Ross, Andy Putz, Mike Rice, Mark Baumgarten</p>
             <p class="font-graphik-regular text-[16px] mb-2"><span class="font-graphik-semibold inline-block pr-2">Design and development</span>Anna Boone, Tom Nehil</p>
-            <p class="font-graphik-regular text-[16px] mb-2"><span class="font-graphik-semibold inline-block pr-2">Promotion</span>Sara Porter</p>
+            <p class="font-graphik-regular text-[16px] mb-2"><span class="font-graphik-semibold inline-block pr-2">Promotion</span>Sara Porter, Mia Andrea</p>
         </div>
     </div>
 </div>
